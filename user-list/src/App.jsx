82afca6f2cff3eda62bd20b1f272027426b7d5be
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-14 11:13:34 +0300

    Move header and footer into separate components.

--- user-list/src/App.jsx
+++ user-list/src/App.jsx
@@ -1,6 +1,16 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles.css'
+
 function App() {
 
-  return 
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
