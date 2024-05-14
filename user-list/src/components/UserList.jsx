fa4410ae4b56c1b21d01d869dfc28cc9110361b0
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-14 12:27:03 +0300

    Move user table row into new component.

--- user-list/src/components/UserList.jsx
+++ user-list/src/components/UserList.jsx
@@ -1,9 +1,13 @@
 import Search from "./Search";
+import UserListTable from "./UserListTable";
 
 const UserList = () => {
     return (
         <section className="card users-container">
             <Search />
+            <UserListTable />
+
+            <button className="btn-add btn">Add new user</button>
         </section>
     );
 }
