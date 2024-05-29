667bfcdb38256ed500f713c782a50f56388a3732
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-29 11:54:24 +0300

    Create Home component and import it to the main file.

--- games-play/client/src/App.jsx
+++ games-play/client/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div id="box">
       <Header />
+      <Home />
     </div>
   );
 }
