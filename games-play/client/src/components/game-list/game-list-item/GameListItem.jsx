f7a840b8158d8257ea70bf69e7bd86a2f689fdd6
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-31 10:35:46 +0300

    Create component with single game item.

---
+++ games-play/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -0,0 +1,12 @@
+export default function GameListItem() {
+  return (
+    <div className="allGames">
+      <div className="allGames-info">
+        <img src="./images/avatar-1.jpg" />
+        <h6>Action</h6>
+        <h2>Cover Fire</h2>
+        <a href="#" className="details-button">Details</a>
+      </div>
+    </div>
+  );
+}
