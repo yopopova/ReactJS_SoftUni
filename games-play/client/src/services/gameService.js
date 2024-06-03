7c843fa904a9ca027d97d82a832b3a8dc610eeab
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-03 13:40:12 +0300

    Create get request for specific game.

--- games-play/client/src/services/gameService.js
+++ games-play/client/src/services/gameService.js
@@ -4,8 +4,12 @@ const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
-
     return Object.values(result);
+}
+
+export const getOne = async (gameId) => {
+    const result = await request.get(`${baseUrl}/${gameId}`);
+    return result;
 }
 
 export const create = async (gameData) => {
