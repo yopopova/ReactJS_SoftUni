1c67839d5766f855136af0038f1de8f993bd4418
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-03 13:41:07 +0300

    Set game details view and routing up.

--- games-play/client/src/components/game-list/game-list-item/GameListItem.jsx
+++ games-play/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -1,16 +1,15 @@
-export default function GameListItem({
-  _id,
-  title,
-  category,
-  imageUrl
-}) {
+import { Link } from "react-router-dom";
+
+export default function GameListItem({ _id, title, category, imageUrl }) {
   return (
     <div className="allGames">
       <div className="allGames-info">
         <img src={imageUrl} />
         <h6>{category}</h6>
         <h2>{title}</h2>
-        <a href="#" className="details-button">Details</a>
+        <Link to={`/games/${_id}`} className="details-button">
+          Details
+        </Link>
       </div>
     </div>
   );
