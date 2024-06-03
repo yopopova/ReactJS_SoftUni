b6682fe83b2ff614c0306f23fe5a7ed18f1c43f4
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-03 11:31:45 +0300

    Set app requests up.

--- games-play/client/src/lib/request.js
+++ games-play/client/src/lib/request.js
@@ -1,8 +1,30 @@
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'content-type': 'application/json'
+        }
+    }
+
+    return options;
+}
+
 export const request = async (method, url, data) => {
     const responce = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
+
+    // ...buildOptions(data) is an object - full or empty, so we destructuring it
 
     const result = await responce.json();
     return result;
 }
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
+export const patch = request.bind(null, 'PATCH');
--- games-play/client/src/services/gameService.js
+++ games-play/client/src/services/gameService.js
@@ -1,23 +1,24 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
-    // console.log(result);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 }
 
 export const create = async (gameData) => {
-    const responce = await fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    });
+    const result = await request.post(baseUrl, gameData);
 
-    const result = await responce.json();
+    // const responce = await fetch(baseUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify(gameData)
+    // });
+    // const result = await responce.json();
+
     return result;
 }
