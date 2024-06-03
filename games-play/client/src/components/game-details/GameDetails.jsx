b27e61c817520aa45d19f2cd501133180a09859c
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-03 14:26:12 +0300

    Set add comments functionality up.

--- games-play/client/src/components/game-details/GameDetails.jsx
+++ games-play/client/src/components/game-details/GameDetails.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import * as gameService from "../../services/gameService"
+import * as gameService from "../../services/gameService";
+import * as commentService from "../../services/commentService";
 
 export default function GameDetails() {
     const [game, setGame] = useState({});
+    const [comments, setComments] = useState([]);
     const { gameId } = useParams();
 
     useEffect(() => {
         gameService.getOne(gameId)
         .then(setGame)
+
+        commentService.getAll()
+        .then(setComments)
     }, [gameId]);
+
+    const addCommentHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const newComment = await commentService.create(gameId, formData.get('username'), formData.get('comment'));
+
+        setComments(state => [...state, newComment]);
+        console.log(newComment);
+    }
 
     return (
         <section id="game-details">
@@ -29,14 +45,16 @@ export default function GameDetails() {
             <div className="details-comments">
                 <h2>Comments:</h2>
                 <ul>
-                    <li className="comment">
-                        <p>Content: I rate this one quite highly.</p>
-                    </li>
-                    <li className="comment">
-                        <p>Content: The best game.</p>
-                    </li>
+                    {comments.map(({_id, username, text}) => (
+                        <li key={_id} className="comment">
+                            <p>{username}: {text}</p>
+                        </li>
+                    ))}
                 </ul>
-                <p className="no-comment">No comments.</p>
+
+                {comments.length === 0 && (
+                    <p className="no-comment">No comments.</p>
+                )}
             </div>
 
             <div className="buttons">
@@ -47,7 +65,8 @@ export default function GameDetails() {
 
         <article className="create-comment">
             <label>Add new comment:</label>
-            <form className="form">
+            <form className="form" onSubmit={addCommentHandler}>
+                <input type="text" name="username" placeholder="username" />
                 <textarea name="comment" placeholder="Comment......"></textarea>
                 <input className="btn submit" type="submit" value="Add Comment" />
             </form>
