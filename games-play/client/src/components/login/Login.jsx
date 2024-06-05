1819b62909460bcb27c608838ead40780061c838
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-05 12:13:31 +0300

    Create auth context and set login up.

--- games-play/client/src/components/login/Login.jsx
+++ games-play/client/src/components/login/Login.jsx
@@ -1,16 +1,17 @@
+import { useContext } from "react";
 import useForm from "../../hooks/useForm";
+import AuthContext from "../../contexts/authContext";
 
 const LoginFormKeys = {
     Email: 'email',
     Password: 'password'
 };
 
-export default function Login({
-    loginSubmitHandler,
-}) {
+export default function Login() {
+    const { loginSubmitHandler } = useContext(AuthContext);
     const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
         [LoginFormKeys.Email]: '',
-        [LoginFormKeys.Password]: ''
+        [LoginFormKeys.Password]: '',
     });
 
     return (
