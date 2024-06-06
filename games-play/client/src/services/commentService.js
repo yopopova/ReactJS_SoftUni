d2dcc006f2860f5c16b9d85a51f4a0f2166da27f
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-06 13:01:31 +0300

    Set comments filtration up.

--- games-play/client/src/services/commentService.js
+++ games-play/client/src/services/commentService.js
@@ -1,17 +1,21 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
+    // With 'query' we will receive ONLY the current game comments! Not all, but ONLY for the current game!
+    // It works only with collections (data), and not with 'jsonstore'.
     const query = new URLSearchParams({
         where: `gameId="${gameId}"`
     });
 
     // ?${query.toString()}
-    const result = await request.get(`${baseUrl}`);
+    const result = await request.get(`${baseUrl}?${query}`);
 
     // TODO: temp solution until migration to collections service
-    return Object.values(result).filter(comment => comment.gameId === gameId);
+    // return result.filter(comment => comment.gameId === gameId);
+    
+    return result;
 };
 
 export const create = async (gameId, username, text) => {
