2e6cd5b686823e4360fbd2d7655e01ca18b54f0a
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-06 10:22:21 +0300

    Meka registration form controlled.

--- games-play/client/src/hooks/useForm.js
+++ games-play/client/src/hooks/useForm.js
@@ -14,6 +14,7 @@ export default function useForm(submitHandler, initialValues) {
 
     const onSubmit = (e) => {
         e.preventDefault();
+        // console.log(values);
         submitHandler(values);
     }
 
