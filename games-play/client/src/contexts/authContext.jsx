1fdcb1d8be972c28cf74fe1a668a703a91e88866
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-07 10:59:35 +0300

    Create auth provider.

--- games-play/client/src/contexts/authContext.jsx
+++ games-play/client/src/contexts/authContext.jsx
@@ -2,6 +2,17 @@ import { createContext } from "react";
 
 const AuthContext = createContext();
 
+export const AuthProvider = ({
+    children,
+    value
+}) => {
+    return(
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
 // We can name contexts, if they are more than one.
 AuthContext.displayName = 'AuthContext';
 
