a236957477b5010f880ba2d3f17c80e15077c257
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-07 13:23:10 +0300

    Move authentication provider into authContext file.

--- games-play/client/src/App.jsx
+++ games-play/client/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
-import * as authService from './services/authService';
 import { AuthProvider } from './contexts/authContext';
 import Path from './paths';
 
@@ -15,52 +13,8 @@ import GameDetails from './components/game-details/GameDetails';
 import Logout from './components/logout/Logout';
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken');
-    return {};
-  });
-
-  //accessToken
-  //email
-  // username
-  //_id
-
-  const loginSubmitHandler = async (values) => {
-    const result = await authService.login(values.email, values.password);
-    console.log(result);
-
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-    navigate(Path.Home);
-  }
-
-  const registerSubmitHandler = async (values) => {
-    // console.log(values);
-    const result = await authService.register(values.email, values.password);
-
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-    navigate(Path.Home);
-  }
-
-  const logoutHandler = () => {
-    setAuth({});
-    // navigate(Path.Home);
-    localStorage.removeItem('accessToken');
-  }
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  }
-
   return (
-    <AuthProvider value={values}>
+    <AuthProvider>
       <div id="box">
         <Header />
 
--- games-play/client/src/contexts/authContext.jsx
+++ games-play/client/src/contexts/authContext.jsx
@@ -1,19 +1,56 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import * as authService from '../services/authService';
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({
-    children,
-    value
-}) => {
-    return(
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState(() => {
+    localStorage.removeItem("accessToken");
+    return {};
+  });
+
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+    console.log(result);
+
+    setAuth(result);
+    localStorage.setItem("accessToken", result.accessToken);
+    navigate(Path.Home);
+  };
+
+  const registerSubmitHandler = async (values) => {
+    // console.log(values);
+    const result = await authService.register(values.email, values.password);
+
+    setAuth(result);
+    localStorage.setItem("accessToken", result.accessToken);
+    navigate(Path.Home);
+  };
+
+  const logoutHandler = () => {
+    setAuth({});
+    // navigate(Path.Home);
+    localStorage.removeItem("accessToken");
+  };
+
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    logoutHandler,
+    username: auth.username || auth.email,
+    email: auth.email,
+    isAuthenticated: !!auth.accessToken,
+  };
+
+  return <AuthContext.Provider value={values}>
+        {children}
+    </AuthContext.Provider>;
+};
 
 // We can name contexts, if they are more than one.
-AuthContext.displayName = 'AuthContext';
+AuthContext.displayName = "AuthContext";
 
 export default AuthContext;
