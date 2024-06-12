1d8cd76d7ab30b1df4fe5ee345c33abd5e9b7466
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-12 14:15:34 +0300

    Create HOC and set navigation up.

--- class-components/client/src/components/Header.jsx
+++ class-components/client/src/components/Header.jsx
@@ -1,35 +1,55 @@
 import { Component } from "react";
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from "@ant-design/icons";
+import { Navigate } from 'react-router-dom';
+
 import { Menu } from "antd";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from "@ant-design/icons";
 
-export default class Header extends Component {
-    render() {
-        const items = [
-            {
-              label: "Navigation One",
-              key: "mail",
-              icon: <MailOutlined />,
-            },
-            {
-              label: "Navigation Two",
-              key: "app",
-              icon: <AppstoreOutlined />,
-              disabled: true,
-            },
-            {
-              key: "alipay",
-              label: (
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                  Navigation Four - Link
-                </a>
-              ),
-            },
-          ];
+import TodoContext from "../context/TodoContext";
+import withNavigate from "../HOC/withNavigate";
 
-        return(
-            <>
-                <Menu mode="horizontal" items={items} />
-            </>
-        );
-    }
+
+class Header extends Component {
+  render() {
+    const items = [
+      {
+        label: "Navigation One",
+        key: "mail",
+        icon: <MailOutlined />,
+      },
+      {
+        label: "Navigation Two",
+        key: "app",
+        icon: <AppstoreOutlined />,
+        disabled: true,
+      },
+      {
+        key: "alipay",
+        label: (
+          <a
+            href="https://ant.design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Navigation Four - Link
+          </a>
+        ),
+      },
+    ];
+
+    // this.props.navigate('/404');
+
+    return (
+      <TodoContext.Consumer>
+        {({ name }) => {
+          <>
+            <Menu mode="horizontal" items={items} />
+
+            <h3>{name}</h3>
+          </>;
+        }}
+      </TodoContext.Consumer>
+    );
+  }
 }
+
+export default withNavigate(Header);
