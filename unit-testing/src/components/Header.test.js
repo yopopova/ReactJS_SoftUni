ae16283d4ccffe6edf03debc2490b19c3661a70a
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-17 10:22:11 +0300

    Set tests up to run together.

--- unit-testing/src/components/Header.test.js
+++ unit-testing/src/components/Header.test.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Header from "./Header";
 import ReactDOM from 'react-dom';
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 
 describe('Header Component', () => {
+    beforeEach(() => {
+        document.body.innerHTML = '';
+        cleanup();
+    });
 
     // FIRST WAY
-    // test('Has heading without testing library', () => {
-    //     const rootElement = document.createElement('div');
-    //     rootElement.id = 'root';
+    test('Has heading without testing library', () => {
+        const rootElement = document.createElement('div');
+        rootElement.id = 'root';
 
-    //     document.body.appendChild(rootElement);
+        document.body.appendChild(rootElement);
 
-    //     ReactDOM.render(<Header />, rootElement);
+        ReactDOM.render(<Header />, rootElement);
 
-    //     const actualElement = rootElement.querySelector('h1.heading');
-    //     expect(actualElement.textContent).toBe('Unit Testing');
-    // });
+        const actualElement = rootElement.querySelector('h1.heading');
+        expect(actualElement.textContent).toBe('Unit Testing');
+    });
 
     // SECOND WAY
     test('Has heading using testing library', () => {
